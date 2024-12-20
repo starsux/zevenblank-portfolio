f622b073911e586d49945f3bd9af797802fd1783
Starsux <lvicmdfs9@gmail.com>
2024-12-20 09:46:38 -0600

    added package speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import GoogleSnippet from "./GoogleSnippet";
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
