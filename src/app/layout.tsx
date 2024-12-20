5700fec6521c5702a38ad6172442ffb396da9c9f
Starsux <lvicmdfs9@gmail.com>
2024-12-20 09:50:51 -0600

    speed-insights in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SpeedInsights/>
       <body className={inter.className}>{children}</body>
       <GoogleSnippet/>
     </html>
