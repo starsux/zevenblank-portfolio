c8648921b92a21a2c086133be40c2d730784f425
Starsux <lvicmdfs9@gmail.com>
2025-06-23 14:39:27 -0600

    Opengraph metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "zevenblank's portfolio",
   description: "Official Art Portfolio by zevenblank.",
+  openGraph:{
+    title: "zevenblank art portfolio",
+    description: "zevenblank art portfolio",
+    images:[{
+      url: "/thumb.png",
+      width: 1366,
+      height: 680,
+      alt: "zevenblank portfolio",
+  }],
+    locale: "en_US",
+    type: "website",
+  },
   verification: {
     other: {
       "p:domain_verify": "c43700326c4b9a5a49583ec073af5e76"
